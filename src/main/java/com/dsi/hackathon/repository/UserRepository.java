70e2f7a0453c2777b3ba2c89b5bece17f45a6813
ornob011 <ornob011@gmail.com>
2025-05-23 00:41:34 +0600

    add-registration: Add method to check if user exist by email

--- src/main/java/com/dsi/hackathon/repository/UserRepository.java
+++ src/main/java/com/dsi/hackathon/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.dsi.hackathon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
 }

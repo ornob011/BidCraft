d344c772a35fc768452cca7cc5b9fcd724eca5cf
ornob011 <ornob011@gmail.com>
2025-05-23 00:40:43 +0600

    add-registration: Fix User table creation

--- src/main/java/com/dsi/hackathon/entity/User.java
+++ src/main/java/com/dsi/hackathon/entity/User.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

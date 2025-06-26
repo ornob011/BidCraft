30f5d6e659bd9ec493cc01dfa69f9ad8daf73970
rashid <rashidul.hasan@dsinnovators.com>
2025-06-26 14:20:07 +0600

    Update doc type

--- src/main/java/com/dsi/hackathon/enums/UploadedDocumentType.java
+++ src/main/java/com/dsi/hackathon/enums/UploadedDocumentType.java
@@ -6,20 +6,20 @@ import lombok.Getter;
 public enum UploadedDocumentType {
     // Documents from Organizer
     REQUEST_FOR_PROPOSAL("Request for Proposal (RFP)"),
-    REQUEST_FOR_TENDER("Request for Tender (RFT)"),
-    SCOPE_OF_WORK("Scope of Work (SOW)"),
     TERMS_OF_REFERENCE("Terms of Reference (TOR)"),
-    INSTRUCTIONS_TO_BIDDERS("Instructions to Bidders"),
-    EVALUATION_CRITERIA("Evaluation Criteria Document"),
-    TENDER_ADVERTISEMENT("Tender Advertisement"),
-    PUBLIC_NOTICE("Public Notice"),
-
-    // Documents from Vendor
-    TECHNICAL_PROPOSAL("Technical Proposal"),
-    FINANCIAL_PROPOSAL("Financial Proposal"),
-    COMPLIANCE_DOCUMENTS("Compliance Documents"),
-    BID_SECURITY("Bid Security"),
-    EARNEST_MONEY_DEPOSIT("Earnest Money Deposit (EMD)"),
+//    REQUEST_FOR_TENDER("Request for Tender (RFT)"),
+//    SCOPE_OF_WORK("Scope of Work (SOW)"),
+//    INSTRUCTIONS_TO_BIDDERS("Instructions to Bidders"),
+//    EVALUATION_CRITERIA("Evaluation Criteria Document"),
+//    TENDER_ADVERTISEMENT("Tender Advertisement"),
+//    PUBLIC_NOTICE("Public Notice"),
+//
+//    // Documents from Vendor
+//    TECHNICAL_PROPOSAL("Technical Proposal"),
+//    FINANCIAL_PROPOSAL("Financial Proposal"),
+//    COMPLIANCE_DOCUMENTS("Compliance Documents"),
+//    BID_SECURITY("Bid Security"),
+//    EARNEST_MONEY_DEPOSIT("Earnest Money Deposit (EMD)"),
 
     // Other documents
     OTHER_ATTACHMENTS("Other Attachments");
